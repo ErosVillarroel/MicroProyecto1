9729eb5d5044edac217d44e4388265921cc516de
tochirito <eros.vilaroel@gmail.com>
2024-02-10 20:32:52 -0400

    desesperacion 2

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//Arrays de combinaciones ganadoras 3x3
+const winningArraySmall = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+
+const winningArraySmallSpecial = [
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+//Arrays de combinaciones ganadoras 4x4
+const winningArrayMedium = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+  [0, 4, 8, 12],
+  [1, 5, 9, 13],
+  [2, 6, 10, 14],
+  [3, 7, 11, 15],
+];
+
+const winningArrayMediumSpecial = [
+  [0, 5, 10, 15],
+  [3, 6, 9, 12],
+];
+
+//Arrays de combinaciones ganadoras 5x5
+const winningArrayLarge = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24],
+  [0, 5, 10, 15, 20],
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24],
+];
+
+const winningArrayLargeSpecial = [
+  [0, 6, 12, 18, 24],
+  [4, 8, 12, 16, 20],
+];
+
 //Guarda el tamano seleccionado por el usuario para el carton
 function setSize(size) {
   localStorage.setItem("size", size);
@@ -80,10 +131,6 @@ function assignEventListeners() {
   cells.forEach((e) => {
     e.addEventListener("click", () => {
       e.classList.add("stricken");
-      
     });
   });
 }
-
-//Verificacion de puntos usando un array?
-
